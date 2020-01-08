46aced463e5e7b1dc196bbb240c397090ad550cc
Ismaild938 <59448842+Ismaild938@users.noreply.github.com>
2020-01-08 07:50:53 +0530

    modified the endpoints as per requirment

--- StudentApp/src/main/java/com/student/service/repository/StudentRepository.java
+++ StudentApp/src/main/java/com/student/service/repository/StudentRepository.java
@@ -14,10 +14,16 @@ import com.student.service.model.Student;
 
 @Repository
 public class StudentRepository {
-	
+
+	final String queryForFindById = "select * from Address where id=?";
+	final static String queryForFindAll = "select * from Student";
+	final String INSERT_QUERY = "insert into student (firstName, lastName,fatherName,motherName) values (?, ?,?,?)";
+	final String UPDATE_QUERY = "update student set fathername = ? where mothername = ?";
+	final String DELETE_QUERY = "delete from student where  firstname= ?";
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	class StudentRowMapper implements RowMapper<Student> {
 		@Override
 		public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -31,21 +37,71 @@ public class StudentRepository {
 		}
 
 	}
-	
+
+	/**
+	 * Insert the data into datbase
+	 * 
+	 * @param student
+	 * @return
+	 */
+	public int saveStudent(Student student) {
+		return jdbcTemplate.update(INSERT_QUERY, student.getFirstName(), student.getLastName(), student.getFatherName(),
+				student.getMotherName());
+	}
+
+	/**
+	 * To perform Update on student table 
+	 * @param student
+	 * @throws Exception 
+	 */
+	public void update(Student student) throws Exception {
+		int status = jdbcTemplate.update(UPDATE_QUERY, student.getFatherName(), student.getMotherName());
+		if (status != 0) {
+			System.out.println("Student data has updated for this student " + student.getFirstName());
+		} else {
+			//we can implement our own custome exception class here
+			throw new Exception("NO data has found for this Student");
+		}
+
+	}
+
+	/**
+	 * To perfrom delete on student table
+	 * @param studentName
+	 * @throws Exception 
+	 */
+	public void deleteEmpById(String studentFirstName) throws Exception {
+		int status = jdbcTemplate.update(DELETE_QUERY, studentFirstName);
+		if (status != 0) {
+			System.out.println("Student info deleted for this student " + studentFirstName);
+		} else {
+			//we can implement our own custome exception class here
+			throw new Exception("No student found for this student name");
+		}
+	}
+
+	/**
+	 * To pull record based on Id
+	 * @param id
+	 * @return
+	 */
 	public Address findById(long id) {
-		return jdbcTemplate.queryForObject("select * from Address where id=?", new Object[] { id },
+		return jdbcTemplate.queryForObject(queryForFindById, new Object[] { id },
 				new BeanPropertyRowMapper<Address>(Address.class));
 	}
-	
+
+	/**
+	 * To pull whole records for specific table
+	 * @return
+	 */
 	public Student findAll() {
-		Student student = jdbcTemplate.query("select * from Student", new StudentRowMapper()).get(0);
+		Student student = jdbcTemplate.query(queryForFindAll, new StudentRowMapper()).get(0);
 		Address address = this.findById(10002);
 		student.setAddress_1(address.getAddress_1());
 		student.setAddress_2(address.getAddress_2());
 		student.setAddress_3(address.getAddress_3());
-		
+
 		return student;
 	}
 
-	
 }
--- StudentApp/src/main/java/com/student/service/web/rest/StudentResource.java
+++ StudentApp/src/main/java/com/student/service/web/rest/StudentResource.java
@@ -3,7 +3,10 @@ package com.student.service.web.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +20,30 @@ public class StudentResource {
 	@Autowired
 	private StudentRepository repository;
 	
-	@GetMapping("/getStudentInfo")
+	@GetMapping("/student/getStudentInfo")
 	public ResponseEntity<Student> getStudentInfo() {
 		
 		return new ResponseEntity<>(repository.findAll(),HttpStatus.OK);
+	}
+	
+	@PostMapping("/student/insert")
+	public ResponseEntity<Integer> saveStudentInfo(Student student) {
+		
+		return new ResponseEntity<>(repository.saveStudent(student), HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<String> updateStudent(Student student) throws Exception{
+		
+		repository.update(student);
+		return new ResponseEntity<>("Updatation completed successfully",HttpStatus.OK);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<String> deleteStudent(Student student) throws Exception{
+		
+		repository.deleteEmpById(student.getFirstName());
+		return new ResponseEntity<String>("Deletion completed sussefully ",HttpStatus.OK);
 	}
 
 }
